f4bf9ba689c5e43933b84ac89f5eba1d12924890
Erixhamucaa1 <122046005+Erixhamucaa1@users.noreply.github.com>
2023-02-17 23:52:35 +0100

    Add files via upload

---
+++ loop1.java
@@ -0,0 +1,16 @@
+package loop1;
+
+public class loop1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number=1;
+		while(number<=20) {
+			System.out.println(number);
+			number++;
+		}
+		
+		
+	}
+
+}
